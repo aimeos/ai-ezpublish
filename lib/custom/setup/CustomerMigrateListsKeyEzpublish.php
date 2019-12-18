df75c8dac0f5f29ed793ae64a74049dda0ef717a
Aimeos <aimeos@aimeos.org>
2019-12-18 19:09:22 +0100

    Migrates ezuser_list key column

---
+++ lib/custom/setup/CustomerMigrateListsKeyEzpublish.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2019
+ */
+
+
+namespace Aimeos\MW\Setup\Task;
+
+
+/**
+ * Updates key columns
+ */
+class CustomerMigrateListsKeyEzpublish extends TablesMigrateListsKey
+{
+	/**
+	 * Returns the list of task names which this task depends on.
+	 *
+	 * @return string[] List of task names
+	 */
+	public function getPreDependencies() : array
+	{
+		return ['TablesMigrateListsKey'];
+	}
+
+
+	/**
+	 * Executes the task
+	 */
+	public function migrate()
+	{
+		$this->msg( 'Update eZ Publish lists "key" columns', 0 ); $this->status( '' );
+
+		$this->process( ['db-customer' => 'ezuser_list'] );
+	}
+}
